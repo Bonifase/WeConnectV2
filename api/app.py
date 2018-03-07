80f57564bc6429800d1862303151f82575a98419
Bonifase <orwabonifase@gmail.com>
2018-03-07 14:43:16 +0300

    [ft-#155774998] test duplicate registration

--- api/app.py
+++ api/app.py
@@ -7,19 +7,23 @@ app = Flask(__name__)
 
 users = []
 
+#Register user and ssaving the details in a list called users
 @app.route('/v1/register_user',  methods = ['POST'])
 def register_user():
     data = request.get_json()
     username = data['username']
     email = data['email']
     password = data['password']
-    user = User(username, email, password)
-    users.append(user)
-    # data = {'example': 'Setting renderers on a per-view basis'}
-    # response = 
+    #check if the user details already in the list, otherwise add the details in the list
+    available_emails = [x.email for x in users]
+    if email in available_emails:
+        return make_response(jsonify({"status": "NOT_ACCEPTABLE", "message": "User Details Exist"}), 406)
+    else:
+        user = User(username, email, password)
+        users.append(user)
     return make_response(jsonify({"status": "ok", "message": "Registered Successful"}), 201)
 
-    # return {'example': 'Setting renderers on a per-view basis'}
+   
 
 
 @app.route('/v1/user_login', methods = ['POST'])
@@ -28,7 +32,7 @@ def user_login():
     username = data['username']
     password = data['password']
     if username in users and password in users:
-        response.status_code = 202
+        return make_response(jsonify({"status": "NOT_ACCEPTABLE", "message": "User Details Exist"}), 406)
     
 
     return response
--- api/test_user.py
+++ api/test_user.py
@@ -10,8 +10,8 @@ class AppTestCase(unittest.TestCase):
     def setUp(self):
         app.app.testing = True
         self.app = app.app.test_client()
-        print('app', self.app)
         self.data = {"username":"john", "email":"email@gmail.com","password":"12345"}
+        self.data2 = {"username":"Bill", "email":"bill@gmail.com","password":"12345"}
        
         
 
@@ -20,6 +20,18 @@ class AppTestCase(unittest.TestCase):
         rs = json.loads(response.data.decode())
         self.assertEqual(rs["message"], "Registered Successful")
         self.assertEqual(response.status_code, 201)
+
+    def test_duplicate_register(self):
+        response1 = self.app.post('/v1/register_user', data = json.dumps(self.data2) , content_type = 'application/json')
+        rs1 = json.loads(response1.data.decode())
+        self.assertEqual(rs1["message"], "Registered Successful")
+        self.assertEqual(response1.status_code, 201)
+        response2 = self.app.post('/v1/register_user', data = json.dumps(self.data2) , content_type = 'application/json')
+        rs2 = json.loads(response2.data.decode())
+        self.assertEqual(rs2["message"], "User Details Exist")
+        self.assertEqual(response2.status_code, 406)
+
+
         
    
     
