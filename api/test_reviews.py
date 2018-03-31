48f1f54e1300aa44320d33a0c8ede32183a1d2dd
Bonifase <orwabonifase@gmail.com>
2018-03-31 15:45:45 +0300

    [ft-#155507366] Apply security to privileged API endpoints using JWT

--- api/test_reviews.py
+++ api/test_reviews.py
@@ -12,26 +12,36 @@ class AppTestCase(unittest.TestCase):
         self.app = app.app.test_client()
         self.data = {"reviewbody":"This is my first best review", "businessid":1}
         self.data2 = {"reviewbody":"This is my second review", "businessid":2}
+        self.data3 = {"username":"Bill", "email":"bill@gmail.com","password":"123456"}
      
 
        
         
 
     def test_reviews(self):
-        response = self.app.post('/api/v2/auth/1/reviews', data = json.dumps(self.data) , content_type = 'application/json')
+        response = self.app.post('/api/v2/auth/login', data = json.dumps(self.data3), content_type = 'application/json')
+        result = json.loads(response.data.decode())
+        user_token = result['user_token']
+        response = self.app.post('/api/v2/auth/1/reviews', headers= {'x-access-token':user_token}, data = json.dumps(self.data) , content_type = 'application/json')
         result = json.loads(response.data.decode())
         self.assertEqual(result["message"], "Review added Successfully")
         self.assertEqual(response.status_code, 201)
 
     def test_duplicate_reviews(self):
-        response = self.app.post('/api/v2/auth/2/reviews', data = json.dumps(self.data2) , content_type = 'application/json')
+        response = self.app.post('/api/v2/auth/login', data = json.dumps(self.data3), content_type = 'application/json')
+        result = json.loads(response.data.decode())
+        user_token = result['user_token']
+        response = self.app.post('/api/v2/auth/2/reviews', headers= {'x-access-token':user_token}, data = json.dumps(self.data2) , content_type = 'application/json')
         result = json.loads(response.data.decode())
         self.assertEqual(result["message"], "Review already Exist, use another description")
         self.assertEqual(response.status_code, 409)
 
     
     def test_myreviews(self):
-        response = self.app.get('/api/v2/auth/1/reviews', data = json.dumps(self.data), content_type = 'application/json')
+        response = self.app.post('/api/v2/auth/login', data = json.dumps(self.data3), content_type = 'application/json')
+        result = json.loads(response.data.decode())
+        user_token = result['user_token']
+        response = self.app.get('/api/v2/auth/1/reviews', headers= {'x-access-token':user_token}, data = json.dumps(self.data), content_type = 'application/json')
         result = json.loads(response.data.decode())
         self.assertEqual(result["message"], "Available reviews")
         self.assertEqual(response.status_code, 200) 
