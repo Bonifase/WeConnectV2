60781ed19455691e1600af2cc0a17cfbad008e16
Bonifase <orwabonifase@gmail.com>
2018-03-31 07:19:40 +0300

    [ch-#155507451] Create PostgresSQL/SQLAlchemy database with table models

--- api/models/review.py
+++
@@ -1,8 +0,0 @@
-class Review:
-    class_counter= 1
-    def __init__(self, reviewbody, businessid):
-        self.reviewbody = reviewbody
-        self.businessid = businessid
-        self.id= Review.class_counter
-        Review.class_counter += 1
-        
