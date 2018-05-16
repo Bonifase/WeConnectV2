0a0decec610b9ac0821ac3b8e17d679655aafb83
Bonifase Orwa <orwabonifase@gmail.com>
2018-05-16 19:22:36 +0300

    fix circle CI error

--- app/__init__.py
+++ app/__init__.py
@@ -19,9 +19,10 @@ app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
 app.config.from_object('config.DevelopmentConfig')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
-	
+
 
 from app import views
